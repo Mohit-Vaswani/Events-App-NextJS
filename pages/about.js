37f66c02c6173442c96d12eaadcefa4a6395baa7
Mohit Vaswani <mohitvaswani010@gmail.com>
2022-12-31 10:33:30 +0530

    Pages and Routing

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>This is an app for finding DJ events and musical events around the globe.</p>
+        <p>Version 1.0.1</p>
+    </div>
+  )
+}

1ac3c253255dcf132adc2db09901e3a77f23204c
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-01-01 10:14:36 +0530

    404 PAGE

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+import styles from "../styles/404.module.css";
+
+
+export default function NotFoundPage() {
+  return (
+    <Layout title="Error Page">
+        <div className={styles.error}>
+            <h1>YOU ARE AT THE WRONG PAGE.</h1>
+            <Link href="/">BACK TO THE <strike>FUTURE</strike> HOME </Link>
+        </div>
+    </Layout>
+  )
+}
